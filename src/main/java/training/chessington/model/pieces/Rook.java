9803307c441b13ad8c78b1c2263b6e3dab3c133b
William Rothery <will.rothery@live.co.uk>
2020-10-20 15:58:44 +0100

    returning functionality until i can add to rook test

--- src/main/java/training/chessington/model/pieces/Rook.java
+++ src/main/java/training/chessington/model/pieces/Rook.java
@@ -16,68 +16,52 @@ public class Rook extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
 
-        // declare and define
-        List<Move> results = new ArrayList<>();
         List<Coordinates> coordList = new ArrayList<>();
+        return null;
 
-        Coordinates up = coords.plus(-1, 0);
-        Coordinates down = coords.plus(1, 0);
-        Coordinates twoUp = coords.plus(-2, 0);
-        Coordinates twoDown = coords.plus(2, 0);
-
-        Move moveUp = new Move(coords, up);
-        Move moveDown = new Move(coords, down);
-        Move moveTwoUp = new Move(coords, twoUp);
-        Move moveTwoDown = new Move(coords, twoDown);
-
-
-        // White rules
-        if (getColour() == PlayerColour.WHITE) {
-            if (up.getRow() >= 0 && board.get(up) == null) { // Rook can't leave the board and there's nothing in front of it
-                try {
-                    results.add(moveUp); // move one space
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    return null;
-                }
-                if (from.getRow() == 6 && board.get(twoUp) == null) { // if the Rook is in the starting block and there's nothing in front of it
-                    results.add(moveTwoUp); // move two spaces
-                }
-            }
-            if (pieceDiagonalUpLeft != null && pieceDiagonalUpLeft.getColour() != PlayerColour.WHITE) { // if there's something in the left diagonal space and it's not white
-                results.add(diagonalUpLeft); // move diagonally left
-            }
-            if (pieceDiagonalUpRight != null && pieceDiagonalUpRight.getColour() != PlayerColour.WHITE) { // if there's something in the left diagonal space and it's not white
-                results.add(diagonalUpRight); // move diagonally right
-            }
-        }
-
-        // Black rules
-        if (getColour() == PlayerColour.BLACK) { // Rook can't leave the board and there's nothing in front of it
-            if (down.getRow() <= 7 && board.get(down) == null) {
-                try {
-                    results.add(moveDown); // move one space
-                } catch(ArrayIndexOutOfBoundsException e) {
-                    return null;
-                }
-                if (from.getRow() == 1 && board.get(twoDown) == null) { // if the Rook is in the starting block and there's nothing in front of it
-                    results.add(moveTwoDown); // move two spaces
-                }
-                if (pieceDiagonalDownLeft != null && pieceDiagonalDownLeft.getColour() != PlayerColour.BLACK) { // if there's something in the left diagonal space and it's not black
-                    results.add(diagonalDownLeft); // move diagonally left
-                }
-                if (pieceDiagonalDownRight != null && pieceDiagonalDownRight.getColour() != PlayerColour.BLACK) { // if there's something in the left diagonal space and it's not black
-                    results.add(diagonalDownRight); // move diagonally right
-                }
-            }
-        }
-        return results;
-    }
-
-    private Piece safeBoardGet(Board board, Coordinates coordinates) {
-        try {
-            return board.get(coordinates);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return null;
-        }
     }
 }
+
+
+
+
+
+
+
+//    Pawn Stuff
+//        declare and define
+//        List<Move> results = new ArrayList<>();
+//        Coordinates coords = new Coordinates(from.getRow(), from.getCol());
+//        List<Coordinates> coordList = new ArrayList<>();
+//
+//        Coordinates up = coords.plus(-1, 0);
+//        Coordinates down = coords.plus(1, 0);
+//        Coordinates twoUp = coords.plus(-2, 0);
+//        Coordinates twoDown = coords.plus(2, 0);
+//
+//        Move moveUp = new Move(coords, up);
+//        Move moveDown = new Move(coords, down);
+//        Move moveTwoUp = new Move(coords, twoUp);
+//        Move moveTwoDown = new Move(coords, twoDown);
+//        // White rules
+//        if (getColour() == PlayerColour.WHITE) {
+//            if (up.getRow() >= 0 && board.get(up) == null) { // Rook can't leave the board and there's nothing in front of it
+//
+//        }
+//
+//        // Black rules
+//        if (getColour() == PlayerColour.BLACK) {
+//            if (down.getRow() <= 7 && board.get(down) == null) { // Rook can't leave the board and there's nothing in front of it
+//
+//        }
+//        return results;
+//    }
+//
+//    private Piece safeBoardGet(Board board, Coordinates coordinates) {
+//        try {
+//            return board.get(coordinates);
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            return null;
+//        }
+//    }
+//}
